271d627d7c5f44108413f341997d1f43c30f4b13
nguyentuakina <nguyentuakinapy@gmail.com>
2024-06-04 21:31:42 +0700

    search

--- src/main/java/com/asmkbw/dao/ProductDAO.java
+++ src/main/java/com/asmkbw/dao/ProductDAO.java
@@ -1,13 +1,22 @@
 package com.asmkbw.dao;
 
 import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 import com.asmkbw.entity.Product;
 
 public interface ProductDAO extends JpaRepository<Product, Integer> {
-	@Query("SELECT o FROM Product o WHERE o.category =?1")
-	List<Product> findByCategoryID(Integer id);
+	@Query("SELECT o FROM Product o WHERE o.category.categoryID = :categoryId")
+	List<Product> findByCategoryID(@Param("categoryId") Integer id);
+
+	@Query("SELECT p FROM Product p LEFT JOIN p.category c LEFT JOIN p.productDetails pd "
+			+ "WHERE p.name LIKE %:keywords% OR c.name LIKE %:keywords% OR p.property LIKE %:keywords% OR pd.color LIKE %:keywords%")
+	Page<Product> searchByMultipleCriteria(@Param("keywords") String keywords, Pageable pageable);
+
+	Page<Product> findAllByNameLike(String keywords, Pageable pageable);
+
+
 }
